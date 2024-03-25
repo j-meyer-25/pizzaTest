bef1cf4a7d0c3170e35f63ab4310d1e36789a8c7
j-meyer-25 <j-meyer.13@onu.edu>
2024-03-25 13:12:35 -0400

    init commit

---
+++ app/src/main/java/com/zybooks/pizzatest/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.zybooks.pizzatest
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.zybooks.pizzatest.R
+import kotlin.math.PI
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val smallPizzaDiameter = findViewById<EditText>(R.id.smallPizzaDiameter)
+        val smallPizzaPrice = findViewById<EditText>(R.id.smallPizzaPrice)
+        val largePizzaDiameter = findViewById<EditText>(R.id.largePizzaDiameter)
+        val largePizzaPrice = findViewById<EditText>(R.id.largePizzaPrice)
+        val calculateButton = findViewById<Button>(R.id.calculateButton)
+        val resultTextView = findViewById<TextView>(R.id.resultTextView)
+
+        calculateButton.setOnClickListener {
+            val smallPizzaArea = PI * (smallPizzaDiameter.text.toString().toDouble() / 2).pow(2)
+            val largePizzaArea = PI * (largePizzaDiameter.text.toString().toDouble() / 2).pow(2)
+
+            val smallPizzaCostPerInch = smallPizzaPrice.text.toString().toDouble() / smallPizzaArea
+            val largePizzaCostPerInch = largePizzaPrice.text.toString().toDouble() / largePizzaArea
+
+            val result = when {
+                smallPizzaCostPerInch < largePizzaCostPerInch -> "Small pizza is a better buy."
+                smallPizzaCostPerInch > largePizzaCostPerInch -> "Large pizza is a better buy."
+                else -> "Both pizzas are the same price per square inch."
+            }
+
+            resultTextView.text = result
+        }
+    }
+}
